01606e098ac774094561b41a47283da5bb1bc883
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-05-11 13:07:33 -0300

    alterações na pagina e criação da discussão

--- alternative.js
+++ alternative.js
@@ -2,3 +2,24 @@ function mostrar(){
     document.getElementById("escondido").style.display = 'block';
     document.getElementById("sumir").style.display = 'none';
 };
+/*
+function esconder(){
+    document.getElementById("escondido").style.display = 'none';
+    document.getElementById("sumir").style.display = 'block';
+}
+*/
+
+function substituirDiv(){
+    document.getElementById("aparecer_div").style.display = 'block';
+    document.getElementById("sumir_div").style.display = 'none';
+};
+
+function Enviar(){
+    document.getElementById("after_envio").style.display = 'block';
+    document.getElementById("aparecer_div").style.display = 'none';
+}
+
+function enviarNova(){
+    document.getElementById("aparecer_div").style.display = 'block';
+    document.getElementById("after_envio").style.display = 'none';
+};
