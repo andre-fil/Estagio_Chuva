d804c5bed45b447130d9cee6eecff03b5a540126
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-05-12 10:32:04 -0300

    parte web finalizada

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ function mostrar(){
     document.getElementById("escondido").style.display = 'block';
     document.getElementById("sumir").style.display = 'none';
 };
-/*
-function esconder(){
-    document.getElementById("escondido").style.display = 'none';
-    document.getElementById("sumir").style.display = 'block';
-}
-*/
+
 
 function substituirDiv(){
     document.getElementById("aparecer_div").style.display = 'block';
@@ -17,9 +12,29 @@ function substituirDiv(){
 function Enviar(){
     document.getElementById("after_envio").style.display = 'block';
     document.getElementById("aparecer_div").style.display = 'none';
+    document.getElementById("after_coment").style.display = 'block';
 }
 
 function enviarNova(){
     document.getElementById("aparecer_div").style.display = 'block';
     document.getElementById("after_envio").style.display = 'none';
+    document.getElementById("after_coment").style.display = 'none';
+   
 };
+
+function apareceResposta(){
+    
+    if( document.getElementById("resposta_complemento").style.display == 'block'){
+        document.getElementById("resposta_complemento").style.display = 'none';
+        document.getElementById("discussao").style.height = '670px';
+    } else{ document.getElementById("resposta_complemento").style.display = 'block';
+    document.getElementById("discussao").style.height = '1250px';
+    }
+
+    
+    
+  
+}
+
+
+
